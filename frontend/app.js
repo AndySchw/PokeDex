06f3b240887bb3c0882e0b923144cdc23f493340
AndySchw <131532743+AndySchw@users.noreply.github.com>
2023-07-20 09:04:29 +0200

    .

--- frontend/app.js
+++ frontend/app.js
@@ -6,11 +6,12 @@ const path = require('path');
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('./public', express.static(path.join(__dirname, './public')));
 
+console.log(__dirname)
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.get('http://backend-1:3001/');
+    const response = await axios.get('http://backend:3001');
     res.render('index', { pokemons: response.data });
   } catch (err) {
     res.status(500).send({ message: err.message });
@@ -19,7 +20,7 @@ app.get('/', async (req, res) => {
 
 app.get('/pokemons/:name', async (req, res) => {
   try {
-    const response = await axios.get(`http://backend-1:3001/pokemons/${req.params.name}`);
+    const response = await axios.get(`http://backend:3001/pokemons/${req.params.name}`);
     const { pokemon, nextPokemonName, previousPokemonName } = response.data;
     res.render('pokemonDetail', { pokemon, nextPokemonName, previousPokemonName });
   } catch (err) {
